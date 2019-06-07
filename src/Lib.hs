0f13676fde0676afbf6bd433045f03781f0f7768
tomek <tomek@localhost.localdomain>
2019-06-07 19:44:31 +0200

    refactoring

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,7 +5,8 @@ module Lib
     ( someFunc
     ) where
 
-import Data.ByteString
+import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (pack)
 import Database.Redis as Redis
 import Control.Monad.IO.Class
 import GHC.Generics
@@ -21,17 +22,21 @@ dbAction :: Connection -> IO ()
 dbAction connection = runRedis connection $ do
   set "hello" $ encode (Tomek 10 10)
   set "world" $ encode (Tomek 20 20)
-  hello <- Redis.get "hello"
+  hello <- Redis.get "hello1"
   world <- Redis.get "world"
   liftIO $ print (deserialize $ hello,deserialize $ world)
 
 deserialize :: Either Reply (Maybe ByteString) -> Either Reply Tomek
-deserialize result = result >>= (\y ->  deserializeTomek $ fromJust y)  
+deserialize result = result >>= (\y ->  deserializeTomek y)  
 
-deserializeTomek:: ByteString -> Either Reply Tomek
-deserializeTomek x =  mapLeft (\_ -> SingleLine "") (S.decode x)   
+deserializeTomek:: Maybe ByteString -> Either Reply Tomek
+deserializeTomek (Just value) =  mapLeft (\error -> SingleLine $ pack error) (S.decode value)
+deserializeTomek Nothing = Left $ SingleLine "empty"
 
-data Tomek = Tomek Int Int
+data Tomek = Tomek {
+  age :: Int,
+  size::Int
+}
   deriving (Show, Generic)
 
 instance Serialize Tomek
